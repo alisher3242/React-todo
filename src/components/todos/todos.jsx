dffe11238255a3073bd5d6b4a27652599df432d1
alisher <alisher.tojimov@yandex.ru>
2022-06-12 21:19:58 +0500

    add sorting

--- src/components/todos/todos.jsx
+++ src/components/todos/todos.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export const Todos = ({ setTodos, todos }) => {
 
     const deleteBtn = (id) => {
@@ -12,23 +14,50 @@ export const Todos = ({ setTodos, todos }) => {
         setTodos([...todos])
     }
 
-    const checkBtn = (evt,id) => {
+    const checkBtn = (evt, id) => {
         const foundIndex = todos.findIndex(todo => todo.id === id)
         todos[foundIndex].isCompleated = evt.target.checked
         setTodos([...todos])
     }
+    const allBtn = () => {
+        setTodos(todos)
+    }
+
+    const isCompleatedBtn = () => {
+        const isCompleateFiltered = todos.filter(todo => todo.isCompleated === true)
+        setTodos(isCompleateFiltered)   
+    }
+    
+    const unCompleatedBtn = () => {
+        const unCompleateFiltered = todos.filter(todo => todo.isCompleated === false)
+        setTodos(unCompleateFiltered)
+    }
+
     return (
-        <ul className="list">
-            {
-                todos.map(todo => (
-                    <li key={todo.id} className="item">
-                        <input checked={todo.isCompleated} onChange={(evt) => checkBtn(evt,todo.id)} type="checkbox" className="check" />
-                        <p className="text" id={todo.isCompleated ? "item-active" : "#"}>{todo.text}</p>
-                        <button className="edit" disabled={todo.isCompleated} onClick={() => editBtn(todo)}>Edit</button>
-                        <button onClick={() => deleteBtn(todo.id)} className="delete">Delete</button>
-                    </li>
-                ))
-            }
-        </ul>
+        <div>
+            <ul className="list">
+                {
+                    todos.map(todo => (
+                        <li key={todo.id} className="item">
+                            <input checked={todo.isCompleated} onChange={(evt) => checkBtn(evt, todo.id)} type="checkbox" className="check" />
+                            <p className="text" id={todo.isCompleated ? "item-active" : "#"}>{todo.text}</p>
+                            <button className="edit" disabled={todo.isCompleated} onClick={() => editBtn(todo)}>Edit</button>
+                            <button onClick={() => deleteBtn(todo.id)} className="delete">Delete</button>
+                        </li>
+                    ))
+                }
+            </ul>
+            <div className="btns">
+                <button onClick={allBtn} className="btn btn-success me-1">All <span>{todos.length}</span></button>
+                <button onClick={isCompleatedBtn} className="btn btn-primary me-1">Compleated <span>
+                    {todos.filter(item => item.isCompleated === true).length}
+                </span></button>
+                <button onClick={unCompleatedBtn} className="btn btn-danger me-1">Uncompleated <span>
+                    {todos.filter(item => item.isCompleated === false).length}
+                </span></button>
+            </div>
+        </div>
+
+
     )
 }
