643f4a177c2f941a5aa324b381a037a1b863ba31
alisher <alisher.tojimov@yandex.ru>
2022-06-12 17:21:00 +0500

    add delete, edit and check

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import "./components/form/form.css"
 import {Form} from "./components/form/form"
 import { Todos } from "./components/todos/todos"
@@ -10,7 +10,7 @@ export const App = () => {
         <div>
             <Form setTodos={setTodos}>
             </Form> 
-            <Todos todos={todos}>
+            <Todos setTodos={setTodos} todos={todos}>
             </Todos>
             {window.localStorage.setItem("keys", JSON.stringify(todos))}
 
--- src/components/todos/todos.jsx
+++ src/components/todos/todos.jsx
@@ -1,13 +1,31 @@
-export const Todos = ({ todos }) => {
+export const Todos = ({ setTodos, todos }) => {
+
+    const deleteBtn = (id) => {
+        const deleteFiltered = todos.filter(todo => todo.id !== id)
+        setTodos(deleteFiltered)
+    }
+
+    const editBtn = (todo) => {
+        const editText = prompt("Edit", todo.text)
+        const foundIndex = todos.findIndex(user => user.id === todo.id)
+        todos[foundIndex].text = editText
+        setTodos([...todos])
+    }
+
+    const checkBtn = (evt,id) => {
+        const foundIndex = todos.findIndex(todo => todo.id === id)
+        todos[foundIndex].isCompleated = evt.target.checked
+        setTodos([...todos])
+    }
     return (
         <ul className="list">
             {
                 todos.map(todo => (
                     <li key={todo.id} className="item">
-                        <input type="checkbox" className="check" />
-                        <p className="text">{todo.text}</p>
-                        <button className="edit">Edit</button>
-                        <button className="delete">Delete</button>
+                        <input checked={todo.isCompleated} onChange={(evt) => checkBtn(evt,todo.id)} type="checkbox" className="check" />
+                        <p className="text" id={todo.isCompleated ? "item-active" : "#"}>{todo.text}</p>
+                        <button className="edit" disabled={todo.isCompleated} onClick={() => editBtn(todo)}>Edit</button>
+                        <button onClick={() => deleteBtn(todo.id)} className="delete">Delete</button>
                     </li>
                 ))
             }
